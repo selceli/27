f811775f1b9c7d948eef55275f3240cfc144fe5e
Selceli <selcanece@gmail.com>
2024-08-12 14:42:48 +0300

    commit2

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    const phoneRegex = /^\+?9?0?\d{10}$/;
+    const phoneRegex = /^(\+|00)?[1-9]\d{1,14}$/;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     const form = document.getElementById("form");
     const formContainer = document.querySelector("#form-container");
     const confirmationContainer = document.getElementById("confirmation-container");
+    const searchbutton = document.getElementById("search");
 
     form.addEventListener("submit", function (event) {
         event.preventDefault();
@@ -67,93 +68,109 @@ document.addEventListener("DOMContentLoaded", function () {
             } else {
                 console.error("emailDisplay element not found!");
             }
+
         }
+        modal.style.display = "block";
     });
 
-    const searchBookButton = document.getElementById("searchBook");
-    let isCancelled = false;
+});
 
-    searchBookButton.addEventListener("click", function () {
-        isCancelled = false;
-        const bookTitle = document.getElementById("bookTitle").value;
-        fetchBooks(bookTitle);
-    });
+const searchBookButton = document.getElementById("searchBook");
+let isCancelled = false;
+const searchbutton = document.getElementById("search");
+searchbutton.classList.add("hidden");
 
-    const clear = document.getElementById("clearSearch");
-    clear.addEventListener("click", function () {
-        const bookTitleInput = document.getElementById("bookTitle");
-        bookTitleInput.value = "";
-        isCancelled = true;
-    });
 
-    async function fetchBooks(title) {
-        const query = title.replace(/\s+/g, '+');
-        const URL = `https://openlibrary.org/search.json?q=${query}`;
-        const resultsContainer = document.getElementById('results-container');
+searchBookButton.addEventListener("click", function () {
+    isCancelled = false;
+    const bookTitle = document.getElementById("bookTitle").value;
+    fetchBooks(bookTitle);
+});
 
-        if (!resultsContainer) {
-            console.error("resultsContainer element not found!");
-            return;
-        }
+const clear = document.getElementById("clearSearch");
+clear.addEventListener("click", function () {
+    const bookTitleInput = document.getElementById("bookTitle");
+    bookTitleInput.value = "";
+    isCancelled = true;
+    const resultsContainerList = document.getElementById('results-container-list');
+    resultsContainerList.textContent = '';
+    const resultsContainer = document.getElementById("results-container");
+    resultsContainer.textContent = "";
+});
 
-        let fetchingInterval;
+async function fetchBooks(title) {
+    const query = title.replace(/\s+/g, '+');
+    const URL = `https://openlibrary.org/search.json?q=${query}`;
+    const resultsContainer = document.getElementById('results-container');
 
-        const startFetchingMessage = () => {
-            let dots = 0;
-            fetchingInterval = setInterval(() => {
-                resultsContainer.innerHTML = 'Waiting for data' + '.'.repeat(dots % 5);
-                dots++;
-            }, 500);
-        };
-
-        startFetchingMessage();
-
-        try {
-            const response = await fetch(URL);
-            if (isCancelled) {
-                clearInterval(fetchingInterval);
-                console.log("Fetch cancelled");
-                resultsContainer.innerHTML = "Fetch cancelled.";
-                return;
-            }
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            const data = await response.json();
-
-            console.log(data);
-            displayResults(data);
-            clearInterval(fetchingInterval);
-
-        } catch (error) {
-            console.error(error);
-            resultsContainer.innerHTML = 'Error fetching data.';
-            clearInterval(fetchingInterval);
-        } finally {
-            console.log('fetch is done.');
-        }
+    if (!resultsContainer) {
+        console.error("resultsContainer element not found!");
+        return;
     }
 
-    function displayResults(data) {
-        const resultsContainerList = document.getElementById('results-container-list');
 
-        resultsContainerList.innerHTML = '';
+    let fetchingInterval;
 
-        if (data.docs.length === 0) {
-            resultsContainerList.innerHTML = 'No results found.';
+    const startFetchingMessage = () => {
+        let dots = 0;
+        fetchingInterval = setInterval(() => {
+            resultsContainer.textContent = 'Waiting for data' + '.'.repeat(dots % 5);
+            dots++;
+        }, 500);
+    };
+
+    startFetchingMessage();
+
+    try {
+        const response = await fetch(URL);
+        if (isCancelled) {
+            clearInterval(fetchingInterval);
+            console.log("Fetch cancelled");
+            resultsContainer.textContent = "Fetch cancelled.";
             return;
         }
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
 
-        data.docs.forEach((book) => {
-            const bookElement = document.createElement('div');
-            bookElement.classList.add('book-item');
-            bookElement.innerHTML = `
-                        <h2><a class="bookKnowledge" href="#">${book.title}</a></h2> 
-                        <div class="book-details"></div>
+        console.log(data);
+        displayResults(data);
+        clearInterval(fetchingInterval);
+
+    } catch (error) {
+        console.error(error);
+        resultsContainer.textContent = 'Error fetching data.';
+        clearInterval(fetchingInterval);
+        resultsContainer.textContent = 'Error fetching data: ' + error.message;
+    } finally {
+        console.log('fetch is done.');
+    }
+}
+
+function displayResults(data) {
+    const resultsContainerList = document.getElementById('results-container-list');
+    const resultsContainer = document.getElementById('results-container');
+    resultsContainerList.textContent = '';
+    resultsContainer.textContent = "";
+
+    if (data.docs.length === 0) {
+        resultsContainerList.textContent = 'No results found.';
+        return;
+    }
+
+
+    data.docs.forEach((book) => {
+        const bookElement = document.createElement('div');
+        bookElement.classList.add('book-item');
+        bookElement.innerHTML = `
+                        <button class="bookKnowledge">${book.title}</button> 
+                        <div class="book-details" style="display: none;"></div>
                     `;
-            const knowledge = bookElement.querySelector(".bookKnowledge");
-            const bookDetails = bookElement.querySelector(".book-details");
+        const knowledge = bookElement.querySelector(".bookKnowledge");
+        const bookDetails = bookElement.querySelector(".book-details");
 
+        if (knowledge) {
             knowledge.addEventListener("click", function (event) {
                 event.preventDefault();
                 if (bookDetails.style.display === "none" || bookDetails.style.display === "") {
@@ -162,60 +179,85 @@ document.addEventListener("DOMContentLoaded", function () {
                     bookDetails.style.display = "none";
                 }
             });
-
-            resultsContainerList.appendChild(bookElement);
-        });
-    }
-    async function fetchBookDetails(bookKey, bookDetailsContainer) {
-        try {
-            const response = await fetch(`https://openlibrary.org${bookKey}.json`);
-
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            const bookData = await response.json();
-            displayBookDetails(bookData, bookDetailsContainer);
-        } catch (error) {
-            console.error('Error fetching book details:', error);
-            bookDetailsContainer.innerHTML = 'Error fetching book details.';
+        } else {
+            console.error("Element with class 'bookKnowledge' not found!");
         }
+        resultsContainerList.appendChild(bookElement);
+    });
+
+
+};
+
+async function fetchBookDetails(bookKey, bookDetailsContainer) {
+    try {
+        const response = await fetch(`https://openlibrary.org${bookKey}.json`);
+
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const bookData = await response.json();
+        displayBookDetails(bookData, bookDetailsContainer);
+    } catch (error) {
+        console.error('Error fetching book details:', error);
+        bookDetailsContainer.textContent = 'Error fetching book details.';
     }
+}
 
 
-    async function fetchAuthorDetails(authorKey) {
-        try {
-            const response = await fetch(`https://openlibrary.org/search/authors.json?q=${authorKey}`);
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            const authorData = await response.json();
-            if (authorData.docs && authorData.docs.length > 0) {
-                return authorData.docs[0].name;
-            } else {
-                return 'Unknown';
-            }
-        } catch (error) {
-            console.error('Error fetching author details:', error);
+async function fetchAuthorDetails(authorKey) {
+    try {
+        const response = await fetch(`https://openlibrary.org/search/authors.json?q=${authorKey}`);
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const authorData = await response.json();
+        if (authorData.docs && authorData.docs.length > 0) {
+            return authorData.docs[0].name;
+        } else {
             return 'Unknown';
         }
+    } catch (error) {
+        console.error('Error fetching author details:', error);
+        return 'Unknown';
     }
-    async function displayBookDetails(book, bookDetailsContainer) {
-        const coverImageUrl = `https://covers.openlibrary.org/b/id/${book.covers ? book.covers[0] : 'default'}-L.jpg`;
-        let authorName = 'Unknown';
+}
+async function displayBookDetails(book, bookDetailsContainer) {
+    const coverImageUrl = `https://covers.openlibrary.org/b/id/${book.covers ? book.covers[0] : 'default'}-L.jpg`;
+    let authorName = 'Unknown';
 
-        if (book.authors && book.authors.length > 0) {
-            authorName = await fetchAuthorDetails(book.authors[0].key);
-        }
+    if (book.authors && book.authors.length > 0) {
+        authorName = await fetchAuthorDetails(book.authors[0].key);
+    }
 
-        const bookDetailsHTML = `
+    const bookDetailsHTML = `
                     <h3>${book.title}</h3>
                     <p>Author: ${authorName}</p>
                     <p>Pages: ${book.number_of_pages || 'Unknown'}</p>
                     <p>Year: ${book.publish_date || 'Unknown'}</p>
                     <img src="${coverImageUrl}" alt="${book.title} cover">
                 `;
-        bookDetailsContainer.innerHTML = bookDetailsHTML;
-        bookDetailsContainer.style.display = "block";
+    bookDetailsContainer.innerHTML = bookDetailsHTML;
+    bookDetailsContainer.style.display = "block";
+}
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+    const search = document.getElementById("search-area");
+    search.classList.remove("hidden");
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+
     }
-});
+}
+
 
